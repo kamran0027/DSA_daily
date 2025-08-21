6ce437cc08bf398cf764037cebd7ae679794d677
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-08-21 23:23:14 +0530

    adding better approach

--- BasicArray/setMatrixZero.java
+++ BasicArray/setMatrixZero.java
@@ -38,6 +38,26 @@ public class setMatrixZero {
     }
 
     //better approachh
-   
-    
+    public void setZeroesExtended(int[][] matrix) {
+        int n=matrix[0].length;
+        int m=matrix.length;
+        int row [] =new int [m];
+        int col []=new int [n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+    }
 }
