0cfa7615e46c6ca371294d433797f385797c6410
Kamran Ahmad <kamranahmadmd463@gmail.com>
2025-08-26 12:28:02 +0530

    commiting..........

--- BasicArray/SubArray.java
+++ BasicArray/SubArray.java
@@ -4,7 +4,6 @@ public class SubArray {
 
     public static ArrayList<ArrayList<Integer>> getSubArray(int a []){
         int n=a.length;
-
         ArrayList<ArrayList<Integer>> result=new ArrayList<>();
         ArrayList<Integer> subArray=new ArrayList<>();
         for(int i=0;i<n;i++){
